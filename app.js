14930e4606e1c1a55e2120587e817c333faf3a97
Yanzen Project <yanwarsolah@gmail.com>
2023-05-25 19:44:22 +0700

    step 02

--- app.js
+++ app.js
@@ -7,40 +7,6 @@ app.set("views", "./views");
 
 app.get("/", (req, res) => {
   res.render("index");
-  //   console.log(req.query);
-
-  //   res.send(
-  //     `Hello Apakabar ${req.query.username} password anda adalah ${req.query.password}`
-  //   );
-});
-
-function getHandlerRequest(req, res) {
-  res.send("Apakabar!");
-}
-
-app.get("/username/:id([0-9]+)", (req, res) => {
-  let username = req.params.id;
-  res.send("Hello Apakabar " + username + "!");
-});
-
-app.get("/contactus", (req, res) => {
-  res.sendFile(__dirname + "/public/contactus.html");
-});
-
-app.use(express.urlencoded({ extended: true }));
-
-app.post("/submit-contactus", (req, res) => {
-  console.log(req.body);
-
-  sendEmail(req.body.name, req.body.subject, req.body.message)
-    .then(() => {
-      res
-        .status(200)
-        .send(`Terima kasih ${req.body.name} telah menghubungi kami`);
-    })
-    .catch((err) => {
-      res.status(500).send(`Ada kesalahan pada server`);
-    });
 });
 
 app.listen(9999, () => {
