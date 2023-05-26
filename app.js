7df7a568a86f8c2be1b5f3d90fafa211d21f127f
Yanzen Project <yanwarsolah@gmail.com>
2023-05-26 19:12:22 +0700

    step 01/02

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const nodemailer = require("nodemailer");
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "./views");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("home");
 });
 
 app.listen(9999, () => {
